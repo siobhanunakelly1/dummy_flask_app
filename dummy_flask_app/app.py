52ad906e630dbf6be36b427da5a092b7837bcd7e
Ben Field <bfield1989@gmail.com>
2018-03-13 16:36:51 +0000

    sync

---
+++ dummy_flask_app/app.py
@@ -0,0 +1,12 @@
+import requests
+from  flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/") 
+def hello():
+    return render_template("hello.html")
+
+
+if __name__ == "__main__":
+    app.run()
