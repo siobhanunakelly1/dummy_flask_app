b7d7a485d84ae317a1388744eb4e74db047c4104
gareth@DESKTOP-UOEVTKM <millerg@tcd.ie>
2018-03-13 15:59:21 +0000

    removed redundant code

--- dummy_flask_app/hello.py
+++ dummy_flask_app/hello.py
@@ -7,14 +7,4 @@ app = Flask("MyApp")
 def hello():
     return render_template("hello.html")
 
-@app.route("/sendmail", methods=["POST"])
-def send_simple_message():
-    form_data = request.form
-    name = form_data["name"]
-    email = form_data["email"]
-    send_to = "{0} <{1}>".format(name, email)
-    email_text = "Hello {0}, this is a mail from me".format(name)
-
-    return "Mail sent - {0}".format(response.text)
-
 app.run()
