17012496ba26d1ac9a587fc72bf87850214156c8
siobhan.kelly8 <c16439762@mydit.ie>
2018-12-06 16:47:06 +0000

    london

--- dummy_flask_app/app.py
+++ dummy_flask_app/app.py
@@ -3,7 +3,6 @@ from  flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-list_of_countries = ["london", "thailand", "sydney", "new york", "rome", "toronto"]
 
 @app.route("/") 
 def hello():
@@ -12,8 +11,22 @@ def hello():
 @app.route("/about") 
 def about():
     return render_template("dummy_file.html")
- 
 
+@app.route("/search") 
+def search():
+    text_box_value = request.GET['text_box']
+    if text_box_value == "London":
+        return render_template("london.html")
+    if text_box_value == "Thailand":
+        return render_template("thailand.html")
+    if text_box_value == "Sydney":
+        return render_template("sydney.html")
+    if text_box_value == "NYC":
+        return render_template("nyc.html")
+    if text_box_value == "Rome":
+        return render_template("rome.html")
+    if text_box_value == "Toronto":
+        return render_template("toronto.html")
 
 if __name__ == "__main__":
     app.run()
